21e92f5c41fb25dfb49c18fc585ad5ae48000c06
Stefan Vojvodic <vojvodicstefan96@gmail.com>
2018-10-07 04:40:47 +0200

    some fixes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 
 import { QrScanner } from '../components/QrScanner';
+import Icon from 'expo/src/Icon';
+import Layout from '../constants/Layout';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -17,43 +19,47 @@ export default class HomeScreen extends React.Component {
 
   state = {
     scannerActive: false,
-    titleText: "Balance for this Week/Month: \n\t\t72.8 / 100"
+    titleText: 'Balance for this Week/Month: \n\t\t72.8 / 100',
   };
 
   _toggleScannerActive = () => {
-    this.setState(
-      (previousState) => {
-        return { scannerActive: !previousState.scannerActive }
-      }
-    )
-  }
+    this.setState(previousState => {
+      return { scannerActive: !previousState.scannerActive };
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        {
-          this.state.scannerActive === true ?
-            <QrScanner /> :
-            <View>
-              <Text>
-                {"\n"}
-                {"\n"}
-                {"\n\t\t"}
-                {this.state.titleText}
-                {"\n"}
-              </Text>
-              <View
-                style={styles.ScanButton}>
-                <Button
-                  onPress={this._toggleScannerActive}
-                  title="Scan to pay"
-                />
-              </View>
+        {this.state.scannerActive === true ? (
+          <View style={{ position: 'relative' }}>
+            
+            <View style={{ position: 'absolute', top: 0, left: 0, width:Layout.window.width, height: Layout.window.height  }}>
+              <QrScanner />
             </View>
-        }
+            <Icon.Ionicons
+              name="md-close"
+              style={{ position: 'absolute', top: 0, left: 0 }}
+              onPress={this._toggleScannerActive}
+              size={40}
+            />
+          </View>
+        ) : (
+          <View>
+            <Text>
+              {'\n'}
+              {'\n'}
+              {'\n\t\t'}
+              {this.state.titleText}
+              {'\n'}
+            </Text>
+            <View style={styles.ScanButton}>
+              <Button onPress={this._toggleScannerActive} title="Scan to pay" />
+            </View>
+          </View>
+        )}
       </View>
     );
-
   }
 
   componentDidMount() {
@@ -69,8 +75,7 @@ export default class HomeScreen extends React.Component {
       this._toggleScannerActive();
     }
     return true;
-  }
-
+  };
 }
 
 const styles = StyleSheet.create({
@@ -79,88 +84,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   ScanButton: {
-    paddingTop: 500,
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+    paddingTop: 50,
   },
 });
